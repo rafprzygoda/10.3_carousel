a9ed56cab4bf4c2df20ebb0d32c7b0a1cd2173be
rafal <ra88fi@o2.pl>
2017-06-08 00:34:13 +0200

    carousel created

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    console.log('DOM loaded');
+    var carouselList = $('#carousel ul');
+
+    function changeSlide() {
+        carouselList.animate({'marginLeft': -300}, 800, moveFirstSlide);
+    };
+    setInterval(changeSlide, 3000);
+    
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    };  
+});
